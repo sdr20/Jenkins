4a85d93c196f1fec3a1be867b210901ddc0200a8
Steven DR <145115889+sdr20@users.noreply.github.com>
2024-05-15 17:03:26 +0800

    script.groovy

--- script.groovy
+++ script.groovy
@@ -1,12 +1,1 @@
-def builapp() {
-    echo 'building the application'
-}
-def builapp() {
-    echo 'Testing the application'
-}
-def builapp() {
-    echo 'deploying the application'
-    echo "deploying version ${params.VERSION}"
-}
 
-return this
