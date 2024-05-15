dc6265045371bc6666da922723c67185855fbfde
Steven DR <145115889+sdr20@users.noreply.github.com>
2024-05-15 16:26:02 +0800

    script.groovy

--- script.groovy
+++ script.groovy
@@ -1,43 +1,12 @@
-def gv
+def builapp() {
+    echo 'building the application'
+}
+def builapp() {
+    echo 'Testing the application'
+}
+def builapp() {
+    echo 'deploying the application'
+    echo "deploying version ${params.VERSION}"
+}
 
-pipeline {
-    agent any
-    parameters {
-        booleanParam(name: 'executeTests', defaultValue: true, description: '')
-    }
-    stages {
-        stage("init") {
-            steps {
-                script {
-                   gv = load "script.groovy" 
-                }
-            }
-        }
-        stage("build") {
-            steps {
-                echo 'building the application...'
-                script {
-                    gv.buildApp()
-                }
-            }
-        }
-        stage("test") {
-                }
-            }
-            steps {
-                echo 'testing the application...'
-                script {
-                    gv.testApp()
-                }
-            }
-        }
-        stage("deploy") {
-            steps {
-                echo 'deplying the application...'
-                echo "deploying version ${params.VERSION}"
-                script {
-                    gv.deployApp()
-                }
-            }
-        }
-    }   
+return this
